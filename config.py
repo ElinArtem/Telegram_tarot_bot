cbdc166a42274fff602da948dec8482e711e92d0
ElinArtem <142169346+ElinArtem@users.noreply.github.com>
2024-07-14 19:04:02 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 settings = {
-    'TOKEN':'7273494958:AAF-9DrGzYjlIzwVO4sM-ywHPSh6cXjxeag',
-    'LOG_FILE':'./3sneakers/sneakbot.log',
-    'CREDS':'./3sneakers/creds.json',
-    'ADMIN': [432657524, 1102084410, 6124302014],
-    'API_GPT': "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"
+    'TOKEN':'Telegram token',
+    'ADMIN': [],
+    'API_GPT': "API key of chat-GPT"
 }
